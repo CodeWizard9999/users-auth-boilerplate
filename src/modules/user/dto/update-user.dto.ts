0778c4a917abc87735e7380196d1268b9fac7a4d
CodeWizard9999 <mONSTER@gmail.com>
2024-04-11 23:21:05 +0300

    fixed change password route validation

--- src/modules/user/dto/update-user.dto.ts
+++ src/modules/user/dto/update-user.dto.ts
@@ -1,7 +1,7 @@
-import { IsNotEmpty, IsString } from 'class-validator';
+import { IsNotEmpty, IsString, IsUUID } from 'class-validator';
 
 export class UpdateUserPasswordDto {
-  @IsString()
+  @IsUUID()
   @IsNotEmpty()
   readonly id: string;
 
